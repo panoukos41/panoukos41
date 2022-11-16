6415bce27d088beca5403dc9b0db0eccbd908e91
Panos <panos_ath@outlook.com>
2022-11-17 00:05:39 +0200

    Update web/js/theme.js

--- web/js/theme.js
+++ web/js/theme.js
@@ -1,33 +1,37 @@
-'use strict'
-const themeKey = 'theme-stylesheet';
-const urlBase = 'https://cdn.jsdelivr.net/npm/water.css@2/out/';
-const stylesheet = document.getElementById(themeKey);
-
-const getTheme = () => window.localStorage.getItem(themeKey);
-
-const setTheme = (theme) => window.localStorage.setItem(themeKey, theme);
-
-const updateTheme = () => {
-    const theme = getTheme();
-    stylesheet.href = `${urlBase}${theme}.min.css`;
-};
-
-const toggleTheme = () => {
-    const newTheme = getTheme() === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-    updateTheme();
-    return newTheme;
-};
-
-if (!getTheme()) {
-    const isLight = window.matchMedia('(prefers-color-scheme: light)').matches;
-    setTheme(isLight ? 'light' : 'dark');
+const Themes = {
+    light: 'light',
+    dark: 'dark'
 }
 
-window.themeKey = themeKey;
-window.getTheme = getTheme;
-window.setTheme = setTheme;
-window.updateTheme = updateTheme;
-window.toggleTheme = toggleTheme;
+// Stylesheets shoud be {url}/theme.min.css where theme is light or dark.
+// this.current = window.matchMedia('(prefers-color-scheme: light)').matches
+class Theme {
+    constructor(stylesheet, url) {
+        this.#stylesheet = stylesheet;
+        this.key = `${stylesheet.id}.theme`;
+        this.url = url.endsWith('/') ? url.slice(0, -1) : url;
 
-updateTheme();
+        this.current = window.matchMedia('(prefers-color-scheme: light)').matches
+            ? Themes.light
+            : Themes.dark;
+
+        stylesheet.href = `${url}${this.current}.min.css`;
+    }
+
+    get current() {
+        return window.localStorage.getItem(key);
+    }
+    set current(value) {
+        window.localStorage.setItem(key, value);
+    }
+
+    // Toggles the theme and returns the new theme found in Themes.
+    toggle() {
+        const theme = this.current === Themes.light ? Themes.dark : Themes.light;
+        this.current = theme;
+        stylesheet.href = `${url}${theme}.min.css`;
+        return theme;
+    }
+}
+
+export { Themes, Theme }
