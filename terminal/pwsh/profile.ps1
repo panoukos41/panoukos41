0f5db06c9aec77319d326bcb9673f6383d0b5b32
Panos <panos_ath@outlook.com>
2023-06-14 00:19:48 +0300

    fix PSReadLine listview style

--- terminal/pwsh/profile.ps1
+++ terminal/pwsh/profile.ps1
@@ -1,12 +1,15 @@
 Import-Module PSReadLine
 Import-Module Terminal-Icons
 
-Set-PSReadLineOption -PredictionViewStyle ListView
 [console]::InputEncoding = [console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
 
 oh-my-posh init pwsh --config "$PSHOME\panos.omp.json" | Invoke-Expression -ErrorAction Ignore 2> $null
 
 $projects = "D:/panou/Projects/"
+
+if ($Host.UI.RawUI.WindowSize.Height -gt 14) {
+    Set-PSReadLineOption -PredictionViewStyle ListView
+}
 
 function elevate () { Start-Process pwsh -Verb runAs }
 
