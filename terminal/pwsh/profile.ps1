2ad4fe351c8a64dbca3e27823b80f2430f9a037c
Panos <panos_ath@outlook.com>
2024-07-27 18:51:56 +0300

    update; profile.ps1

--- terminal/pwsh/profile.ps1
+++ terminal/pwsh/profile.ps1
@@ -1,7 +1,8 @@
-Import-Module PSReadLine -ErrorAction SilentlyContinue
-Import-Module Terminal-Icons -ErrorAction SilentlyContinue
+Import-Module PSReadLine -ErrorAction SilentlyContinue -Force
+Import-Module Terminal-Icons -ErrorAction SilentlyContinue -Force
 
 Set-Alias -Name "k" -Value "kubectl"
+Set-Alias -Name "h" -Value "helm"
 
 [console]::InputEncoding = [console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
 
@@ -13,7 +14,9 @@ if ($Host.UI.RawUI.WindowSize.Height -gt 14) {
     Set-PSReadLineOption -PredictionViewStyle ListView
 }
 
-function elevate () { Start-Process pwsh -Verb runAs }
+function elevate () { 
+    Start-Process pwsh -Verb runAs
+}
 
 function Path-Reload() {
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
